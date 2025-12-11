67612db35bc261f9bfe02c405a921d75c37078e9
Tabish Rafiq <34790598+TabishRafiq@users.noreply.github.com>
2025-12-12 01:30:18 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE = 'site-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png'];
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png'];
 
 self.addEventListener('install', evt => {
   evt.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
